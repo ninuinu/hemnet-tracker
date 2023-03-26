8f1fecf07f3b6c8f6744cda018fe322b788557f8
ninuinu <helena.rosenzw@gmail.com>
2023-03-26 21:18:48 +0200

    Add getListings controller and service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  async getListings(url: string){
+    const response$ = this.httpService.get(url);
+    const response = await firstValueFrom(response$);
+    const $ = cheerio.load(response.data);
+    const listings = [];
+    
+    $('li.normal-results__hit').each((_i, element) => {
+      const listing = {};
+
+      const title = $(element).find('h2.listing-card__street-address');
+      if (title) {
+        listing['title'] = title.text().trim();
+      }
+
+      const price = $(element).find('div.listing-card__attribute--square-meter-price');
+      if (price) {
+        listing['squareMeterPrice'] = price.text().trim();
+      }
+
+      listings.push(listing);
+    });
+
+    return listings;
   }
 }
