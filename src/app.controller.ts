a0901744cba42b35c84adb4113dbfbd938b5d855
ninuinu <helena.rosenzw@gmail.com>
2023-04-16 21:34:20 +0200

    add new route for image upload in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Get, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { S3Service } from './services/s3.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly s3Service: S3Service) {}
 
   @Get('scrape')
   async getListings(@Query('location_ids') location: string) {
     return this.appService.getListings(location);
+  }
+
+  @Post('uploadImage')
+  async uploadImage(@Query('key') key: string, @Query('data') data: Buffer){
+    return this.s3Service.uploadImage(key, data);
   }
 
   @Post('upload')
