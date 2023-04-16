ed867f998dd176d5cee7c4b85290b0856f39f335
ninuinu <helena.rosenzw@gmail.com>
2023-04-16 21:26:44 +0200

    add config variable references to s3 service

--- src/services/s3.service.ts
+++ src/services/s3.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { S3 } from 'aws-sdk';
 
 @Injectable()
 export class S3Service {
   private readonly s3: S3;
+  private readonly bucketName: string;
 
-  constructor() {
-    this.s3 = new S3();
+  constructor(private configService: ConfigService) {
+    this.bucketName = configService.get<string>('S3_BUCKET_NAME');
+    const region = configService.get<string>('AWS_REGION');
+
+    this.s3 = new S3({ region });
   }
 
   async uploadImage(key: string, data: Buffer): Promise<void> {
   
-    const bucketName = '';
-
     const params: S3.PutObjectRequest = {
-      Bucket: bucketName,
+      Bucket: this.bucketName,
       Key: key,
       Body: data,
     };
 
     try {
       await this.s3.putObject(params).promise();
-      console.log(`Successfully uploaded ${key} to ${bucketName}`);
+      console.log(`Successfully uploaded ${key} to ${this.bucketName}`);
     } catch (error) {
       console.error('Failed to upload file to S3:', error.message);
     }
