b422551c20738e7078877ad006f94b91271bffcb
ninuinu <helena.rosenzw@gmail.com>
2023-05-21 21:57:42 +0200

    replace general string type with specifically defined expected date values

--- src/listing/types/DateInWords.type.ts
+++ src/listing/types/DateInWords.type.ts
@@ -1,5 +1,54 @@
+export type day =
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | '10'
+  | '11'
+  | '12'
+  | '13'
+  | '14'
+  | '15'
+  | '16'
+  | '17'
+  | '18'
+  | '19'
+  | '20'
+  | '21'
+  | '22'
+  | '23'
+  | '24'
+  | '25'
+  | '26'
+  | '27'
+  | '28'
+  | '29'
+  | '30'
+  | '31';
+
+export type month =
+  | 'januari'
+  | 'februari'
+  | 'mars'
+  | 'april'
+  | 'maj'
+  | 'juni'
+  | 'juli'
+  | 'augusti'
+  | 'september'
+  | 'oktober'
+  | 'november'
+  | 'december';
+
+export type year = '2022' | '2023';
+
 export type DateInWords = {
-  day: string;
-  month: string;
-  year: string;
+  day: day;
+  month: month;
+  year: year;
 };
