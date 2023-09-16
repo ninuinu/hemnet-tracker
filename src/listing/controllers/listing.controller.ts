8eb40bc96322c27ed1d00aa2d9cb31833ce39721
ninuinu <helena.rosenzw@gmail.com>
2023-09-16 23:01:25 +0200

    add a get most recent occurances endpoint and an endpoint for populating the table

--- src/listing/controllers/listing.controller.ts
+++ src/listing/controllers/listing.controller.ts
@@ -10,6 +10,15 @@ export class ListingController {
     return this.listingService.scrape(location);
   }
 
+  @Get('unique')
+  getAllUnique(@Query('page') page: number, @Query('limit') limit: number) {
+    // TODO add DTO for transformation
+    page = Number(page);
+    limit = Number(limit);
+
+    return this.listingService.getAllUnique(page, limit);
+  }
+
   @Get()
   getAll(@Query('page') page: number, @Query('limit') limit: number) {
     // TODO add DTO for transformation
@@ -19,8 +28,14 @@ export class ListingController {
     return this.listingService.getAll(page, limit);
   }
 
+  @Get('populate')
+  populateUniqueListings() {
+    return this.listingService.populateUniqueListings();
+  }
+
   @Get('matches')
   getMatches(@Query('id') id: string) {
+    console.log('Getting matches for id:', id);
     return this.listingService.getMatches(+id);
   }
 
